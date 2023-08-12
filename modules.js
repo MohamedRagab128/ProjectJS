647e0f822632dfac943fb0e3a8ed41ff9de135e3
Mohamed Ragab <139262778+MohamedRagab128@users.noreply.github.com>
2023-08-12 20:43:45 +0800

    Add files via upload

---
+++ modules.js
@@ -0,0 +1,12 @@
+const name1 = "Ali";
+const name2 = "Reham";
+
+// Not View 
+const LastName = "Elbahouty";
+
+//const name1 = "Ali";
+//const name2 = "Reham";
+
+module.exports = {name1, name2};
+
+//console.log(module);
---
+++ multNumber.js
@@ -0,0 +1,8 @@
+const num1 = 15;
+const num2 = 7;
+
+function multNumber() {
+    console.log(`The Sum Of Number: ${num1 + num2}`);
+}
+
+multNumber();
